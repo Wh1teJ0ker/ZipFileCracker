cd2ea74dfd6d55dc5c97e7b0e244e54350f33728
CloneWith <clonewith@qq.com>
2024-10-19 15:36:52 +0800

    Support exiting gently by cli module

--- zipCracker/util/cli.py
+++ zipCracker/util/cli.py
@@ -1,4 +1,5 @@
 """Simple command line handling."""
+import sys
 
 from zipCracker.util import COLORS
 from zipCracker.util import logger
@@ -20,7 +21,7 @@ SYM: dict[str, str] = {
 }
 
 
-def print_and_log(msg: str, level: str = "Info", module: str = "Generic", nolog: bool = False):
+def print_and_log(msg: str, level: str = "info", module: str = "Generic", nolog: bool = False):
     """
     Print prettified message and log it when necessary.
     """
@@ -34,3 +35,18 @@ def print_and_log(msg: str, level: str = "Info", module: str = "Generic", nolog:
         except AttributeError:
             logger.warn(f"The log level {level} is invalid. Falling back to information level.", __name__)
             logger.info(msg, module)
+
+
+def exit_with_code(code: int):
+    """
+    Make the program exit gently with an exit code.
+
+    This function would 
+    """
+    if code is not 0:
+        logger.error(f"An error occurred, exiting with code {code}.", __name__)
+    else:
+        logger.info(f"Program exiting with code {code}.", __name__)
+
+    logger.cleanup()
+    sys.exit(code)
