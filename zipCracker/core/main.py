a6eaa7db9663b4058e289993bd4c580ca13e7893
CloneWith <clonewith@qq.com>
2024-10-19 14:09:14 +0800

    Add version and help command handling

--- zipCracker/core/main.py
+++ zipCracker/core/main.py
@@ -1,10 +1,14 @@
 """Main process unit."""
 
 import sys
-from zipCracker import __version__, __authors__, __license__
 from zipCracker.util import logger
 # from zipCracker.util import cli
 from zipCracker.util import docs
+from zipCracker.util import cli
+
+# A module must be imported to be found by getattr.
+import zipCracker.modules as modules
+from zipCracker.modules import test
 
 
 def main():
@@ -12,4 +16,26 @@ def main():
     logger.debug(f"Command line arguments: {args}", "Main")
 
     if len(args) == 0:
-        docs.printVersion()
+        cli.print_and_log(msg="Specify a command first. Execute with -h to get a list of commands.",
+                          level="warn", module=__name__)
+    else:
+        if args.__contains__("--version"):
+            if len(args) > 1:
+                cli.print_and_log(msg="Too many arguments for --version", level="error", module=__name__)
+            else:
+                docs.print_version()
+        elif args.__contains__("--help"):
+            # TODO: Get help content for individual modules
+            docs.print_help()
+
+
+def get_module(name: str):
+    """
+    Returns a module with the given name.
+    If the module is not found, None is returned.
+    """
+    try:
+        return getattr(modules, name)
+    except AttributeError:
+        logger.error(f"Module not found: {name}", __name__)
+        return None
--- zipCracker/util/docs.py
+++ zipCracker/util/docs.py
@@ -4,18 +4,16 @@ import zipCracker
 
 DESCRIPTION = "A simple zip file cracker."
 
-def printVersion():
+
+def print_version():
     print(
-        """ZipFileCracker version {0}
-By {1}, licensed under {2}
-{3}"""
-        .format(
-            zipCracker.__version__,
-            ", ".join(zipCracker.__authors__),
-            zipCracker.__license__,
-            DESCRIPTION
-        )
+        f"""ZipFileCracker version {zipCracker.__version__}
+By {" & ".join(zipCracker.__authors__)}, licensed under {zipCracker.__license__}
+{DESCRIPTION}
+"""
     )
 
-def printHelp():
+
+def print_help():
+    # TODO
     return
